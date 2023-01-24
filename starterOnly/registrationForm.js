ea73fd80256f66f0e19cfa1700e8b45488b79784
Nohavye <noha.poncelet@gmail.com>
2023-01-24 11:32:41 +0100

    Implémentation des entrées du formulaire. Code définitif, à refactoriser. (à voir)

--- starterOnly/registrationForm.js
+++ starterOnly/registrationForm.js
@@ -5,17 +5,39 @@ const dElements = {
   modalContent: document.querySelector(".content"),
   openModalButtons: document.querySelectorAll(".modal-btn"),
   closeModalButton: document.querySelector(".close"),
-
-  // Form Elements
-  firstNameInput: document.querySelector(".text-first"),
-  lastNameInput: document.querySelector(".text-last"),
-  emailInput: document.querySelector(".text-email"),
-  birthdateInput: document.querySelector(".text-birthdate"),
-  quantityInput: document.querySelector(".text-quantity"),
-  checkBoxInput: document.querySelectorAll(".checkbox-input")
 }
 
+let map_formElements = new Map();
+
+const firstName = document.querySelector(".text-first");
+map_formElements.set(firstName, { type: 'text', regularExpression: /^[a-zA-z]{2}/, required: true, validated: false });
+
+const lastName = document.querySelector(".text-last");
+map_formElements.set(lastName, { type: 'text', regularExpression: /^[a-zA-z]{2}/, required: true, validated: false });
+
+const eMail = document.querySelector(".text-email");
+map_formElements.set(eMail, { type: 'text', regularExpression: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, required: true, validated: false });
+
+const birthdate = document.querySelector(".text-birthdate");
+map_formElements.set(birthdate, { type: 'text', regularExpression: /.{1}/, required: true, validated: false });
+
+const quantity = document.querySelector(".text-quantity");
+map_formElements.set(quantity, { type: 'text', regularExpression: /^[0-9]/, required: true, validated: false });
+
+const checkBoxsLocation = document.querySelectorAll(".checkbox-input-location");
+map_formElements.set(checkBoxsLocation, { type: 'checkboxlist', required: true, validated: false });
+
+const checkBoxTerms = document.querySelector(".checkbox-input-terms");
+map_formElements.set(checkBoxTerms, { type: 'checkbox', required: true, validated: false });
+
+const checkBoxInputNewsLetters = document.querySelector(".checkbox-input-newsletters");
+map_formElements.set(checkBoxInputNewsLetters, { required: false, validated: false });
+
+const submitButton = document.querySelector(".btn-submit");
+map_formElements.set(submitButton, { required: false, validated: false });
+
 const timeoutClose = 500;
+
 
 export class RegistrationModal {
 
@@ -30,19 +52,33 @@ export class RegistrationModal {
     dElements.closeModalButton.addEventListener("click", this.#visibleModal.bind(this, false));
 
     // Form Events
-    this.#testInput(dElements.firstNameInput, /^[a-zA-z]{2}/);
-    this.#testInput(dElements.lastNameInput, /^[a-zA-z]{2}/);
-    this.#testInput(dElements.emailInput, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-    this.#testInput(dElements.birthdateInput, /.{1}/);
-    this.#testInput(dElements.quantityInput, /^[0-9]/);
+    map_formElements.forEach((value, key, map) => {
 
-    dElements.firstNameInput.addEventListener("input", this.#testInput.bind(this, dElements.firstNameInput, /^[a-zA-z]{2}/));
-    dElements.lastNameInput.addEventListener("input", this.#testInput.bind(this, dElements.lastNameInput, /^[a-zA-z]{2}/));
-    dElements.emailInput.addEventListener("input", this.#testInput.bind(this, dElements.emailInput, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/));
-    dElements.birthdateInput.addEventListener("input", this.#testInput.bind(this, dElements.birthdateInput, /.{1}/));
-    dElements.quantityInput.addEventListener("input", this.#testInput.bind(this, dElements.quantityInput, /^[0-9]/));
+      switch(value.type) {
 
-    dElements.checkBoxInput.forEach((checkbox) => checkbox.addEventListener("input", this.#testCheckBoxInputs));
+        case 'text':
+          this.#testInput_text(key, value);
+          key.addEventListener("input", this.#testInput_text.bind(this, key, value));
+          break;
+
+        case 'checkboxlist':
+          this.#testInput_checkBoxList(key, value);
+          key.forEach((checkbox) => checkbox.addEventListener("input", this.#testInput_checkBoxList.bind(this, key, value)));
+          break;
+
+        case 'checkbox':
+          this.#testInput_checkBox(key, value);
+          key.addEventListener("input", this.#testInput_checkBox.bind(this, key, value));
+          break; 
+
+        default:
+          //console.log('default');
+      }
+
+    });
+
+
+    // this.#formIsValid();
   }
 
   #visibleModal(isVisible) {
@@ -63,42 +99,60 @@ export class RegistrationModal {
     }
   }
 
-  #testInput(inputElement, regularExpression) {
+  #testInput_text(key, value) {
 
-    if(inputElement.value != "" && this.#validateInputContent(inputElement.value, regularExpression)) {
-      inputElement.style.color = "black";
-      inputElement.style.border = "0px";
+    if(key.value != "" && value.regularExpression.test(key.value)) {
+      key.style.color = "black";
+      key.style.border = "0px";
+      value.validated = true;
     } else {
-      inputElement.style.color = "red";
-      inputElement.style.border = "2px solid red";
+      key.style.color = "red";
+      key.style.border = "2px solid red";
+      value.validated = false;
     }
+
+    this.#checkFormValidity();
   }
 
-  #validateInputContent(inputContent, regularExpression) {
-    return regularExpression.test(inputContent);
+  #testInput_checkBoxList(key, value) {
+
+    value.validated = false;
+
+    key.forEach((checkbox) => { if(checkbox.checked) {
+      value.validated = true;
+    }});
+
+    this.#checkFormValidity();
   }
 
-  #test() {
-    return true;
+  #testInput_checkBox(key, value) {
+    value.validated = key.checked;
+    this.#checkFormValidity();
   }
 
-  #testCheckBoxInputs() {
-    
-    let oneIsChecked = false;
-    let checkbox;
+  #checkFormValidity() {
 
-    for(let i = 1; i < 7; i++) {
+    console.log('--------------------------------------------');
+    console.log('--------------------------------------------');
+    for(let value of map_formElements.values()) {
+      if(value.required) { console.log(value); }
+    }
 
-      checkbox = document.querySelector(`#location${i}`);
-      
-      if(checkbox.checked) {
-        console.log(`#location${i} is checked`);
-        oneIsChecked = true;
-        break;
+    let formIsValid = true;
+
+    for(let value of map_formElements.values()) {
+      if(value.required) {
+        if(!value.validated) {
+          formIsValid = false;
+          break;
+        }
       }
     }
 
-    return oneIsChecked;
+    if(formIsValid) {
+      submitButton.style.opacity = "1";
+    } else {
+      submitButton.style.opacity = "0.25";
+    }
   }
-
 }
